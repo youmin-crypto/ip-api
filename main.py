d0ce38eb37cbdc4992dc5103c06dace0ff51cba8
youmin-crypto <you000957@gmail.com>
2026-02-13 21:49:38 +0630

    Update main.py

--- main.py
+++ main.py
@@ -2,43 +2,31 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# IP တွေကို စာရင်း (List) အနေနဲ့ သိမ်းမယ်
-device_ips = {
-    "phone1": [],
-    "phone2": [],
-    "phone3": [],
-    "phone4": [],
-    "phone5": []
-}
+device_ips = {f"phone{i}": [] for i in range(1, 6)}
 
 @app.route('/')
 def home():
     return "Running!"
-    
-@app.route('/update', methods=['GET'])
+
+@app.route('/get-all')
+def get_all():
+    return jsonify(device_ips)
+
+@app.route('/update')
 def update_ip():
     device_id = request.args.get('id')
     new_ip = request.args.get('ip')
-    
     if device_id in device_ips:
-        # IP အသစ်ကို စာရင်းထဲမှာ ထပ်တိုး (Append) လုပ်မယ်
-        # အကယ်၍ IP က ရှိပြီးသားဆိုရင် ထပ်မထည့်အောင် စစ်လို့ရပါတယ်
         if new_ip not in device_ips[device_id]:
             device_ips[device_id].append(new_ip)
-        return f"Added {new_ip} to {device_id} list", 200
-    return "Invalid Device ID", 400
+        return f"OK", 200
+    return "Error", 400
 
-# IP အားလုံးကို ပြန်ကြည့်ရန် Link: /get-all
-@app.route('/get-all', methods=['GET'])
-def get_all_ips():
-    return jsonify(device_ips)
-
-# နေ့စဉ် Reset လုပ်ရန် သို့မဟုတ် စာရင်းရှင်းရန်: /reset-all
 @app.route('/reset-all')
-def reset_ips():
+def reset():
     global device_ips
     device_ips = {f"phone{i}": [] for i in range(1, 6)}
-    return "All IP lists have been cleared."
-    
+    return "Reset OK"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
